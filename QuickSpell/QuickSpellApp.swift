951c9502e1a4264b110f51bb28383d3c57a0b189
Robertm339 <71312299+Robertm339@users.noreply.github.com>
2022-11-03 17:55:24 -0700

    Initial Commit

---
+++ QuickSpell/QuickSpellApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickSpellApp.swift
+//  QuickSpell
+//
+//  Created by Robert Martinez on 11/3/22.
+//
+
+import SwiftUI
+
+@main
+struct QuickSpellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
